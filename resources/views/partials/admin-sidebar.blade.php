97ec4d7b9122c03f076e01b24c1dfd533aa04117
Amour SODJINOU <amoursodjinou@gmail.com>
2023-05-10 18:19:54 +0100

    sidbar titles

--- resources/views/partials/admin-sidebar.blade.php
+++ resources/views/partials/admin-sidebar.blade.php
@@ -6,33 +6,33 @@
                 <span class="nav_logo-name">V-Drivers</span> 
             </a>
             <div class="nav_list">
-                 <a href="#" class="nav_link active"> 
+                 <a href="#" class="nav_link active" title="Tableau de bord"> 
                     <i class='fa-solid fa-border-all nav_icon'></i>
                     <span class="nav_name">Tableau de bord</span> 
                 </a> 
-                <a href="#" class="nav_link">
+                <a href="#" class="nav_link" title="Conducteurs">
                     <i class='fa-solid fa-user-tie nav_icon'></i> 
                     <span class="nav_name">Conducteurs</span> 
                 </a> 
-                <a href="#" class="nav_link"> 
+                <a href="#" class="nav_link" title="Propriétaires"> 
                     <i class='fa-solid fa-user nav_icon'></i>
                     <span class="nav_name">Propriétaires</span> 
                 </a> 
-                <a href="#" class="nav_link"> 
+                <a href="#" class="nav_link" title="FAQs"> 
                     <i class='fa-solid fa-clipboard-question nav_icon'></i>
                     <span class="nav_name">FAQs</span> 
                 </a> 
-                <a href="#" class="nav_link">
+                <a href="#" class="nav_link" title="Utilisateurs">
                     <i class='fa-solid fa-users nav_icon'></i> 
                     <span class="nav_name">Utilisateurs</span> 
                 </a> 
-                <a href="#"
-                    class="nav_link"> <i class='fa-solid fa-newspaper nav_icon'></i> 
+                <a href="#" class="nav_link" title="Newletter"> 
+                    <i class='fa-solid fa-newspaper nav_icon'></i> 
                     <span class="nav_name">Newletter</span> 
                 </a>
             </div>
         </div> 
-        <a href="#" class="nav_link"> 
+        <a href="#" class="nav_link" title=""> 
             <i class='fa-solid fa-right-from-bracket nav_icon'></i>
             <span class="nav_name">Déconnexion</span> 
         </a>
